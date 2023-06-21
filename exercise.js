531b07d4e759b143a5a51f34789f70b28ce6b426
Mohammed Sharafi <37874836+Pr1ntGn0me@users.noreply.github.com>
2023-06-21 21:08:27 +0200

    commit middle

--- exercise.js
+++ exercise.js
@@ -79,3 +79,23 @@ let isSameTree = function (p, q) {
 
   return leftSame && rightSame;
 };
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+let plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+};
+
+let digits = [9];
+
+let result = plusOne(digits);
+console.log(result);
